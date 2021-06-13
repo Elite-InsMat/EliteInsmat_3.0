f0b45520031b86a2438dc22b37b387b486d6060d
Juhana Kuparinen <juhku@elisanet.fi>
2021-06-14 00:59:50 +0300

    vaihdettu landing page kuva käyttämään graphql:llää

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,41 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 
 import '../styles/index.css';
-import haalarimerkki from '../images/InsmatHaalarimerkki.png';
 
-const IndexPage = (): JSX.Element => (
-  <Layout>
-    <Seo title="Home" />
-    <div className="Home">
-      <header>
-        <h1>Elite InsMat</h1>
-        <h2>meemihtävä opiskelijaporukka Turun yliopistosta</h2>
-        <img src={haalarimerkki} alt='haalarimerkki' />
-        <h3>insmatit on muuten läpi :)</h3>
-      </header>
-    </div>
-  </Layout>
-);
+const IndexPage = (): JSX.Element => {
+  const result = useStaticQuery<Data>(graphql`
+    query {
+      file(name: { eq: "logo" }){
+        publicURL
+      }
+    }
+  `);
+
+  console.log(result);
+
+  return(
+    <Layout>
+      <Seo title="Home" />
+      <div className="Home">
+        <header>
+          <h1>Elite InsMat</h1>
+          <h2>meemihtävä opiskelijaporukka Turun yliopistosta</h2>
+          <img src={result.file.publicURL} alt='haalarimerkki' />
+          <h3>insmatit on muuten läpi :)</h3>
+        </header>
+      </div>
+    </Layout>
+  );
+};
+
+type Data = {
+    file: {
+      publicURL: string;
+    }
+};
 
 export default IndexPage;
